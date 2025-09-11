da13047465b1e0bdcb5bb3d3b13f146b7e90bb35
eronhavolli <eronhavolli@gmail.com>
2025-09-11 16:43:19 +0200

    test

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const prettier = require("eslint-plugin-prettier");
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ["dist/*"],
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error", // Active Prettier comme règle ESLint
+      "no-unused-vars": "warn",     // Avertit si tu déclares une variable non utilisée
+    },
+  },
+]);
+
